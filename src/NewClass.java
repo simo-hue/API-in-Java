70c8ed3fdbe7f654da84f4d61b8c2d1c43a23de3
Mattioli Simone <mattioli.simone.10@gmail.com>
2022-01-17 15:05:19 +0100

    starting code
    creating new file and start to write some code

---
+++ src/NewClass.java
@@ -0,0 +1,21 @@
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author simonemattioli
+ */
+public class NewClass {
+    public static void main(String[] args) throws MalformedURLException {
+        //api e link
+        //https://www.metaweather.com/api/location/search/?query=<>
+        
+        URL url = new URL("https://www.metaweather.com/api/location/search/?query=");
+    }
+}
