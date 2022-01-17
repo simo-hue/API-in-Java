6e7e63091d5a4fac4d565845b09b6c151ad28e06
Mattioli Simone <mattioli.simone.10@gmail.com>
2022-01-17 15:19:41 +0100

    Update NewClass.java

--- src/NewClass.java
+++ src/NewClass.java
@@ -4,11 +4,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import jdk.jshell.spi.ExecutionControl;
 
 /**
  *
@@ -23,6 +19,7 @@ public class NewClass {
         String Città = "";
         int Risposta;
         
+        System.out.println("Digitare il ");
         //imposto l'URL dalla quale ricevero l'array in formato JSON
         URL url = new URL("https://www.metaweather.com/api/location/search/?query=");
         
@@ -34,8 +31,20 @@ public class NewClass {
         //Tramite la variabile risposta controllo se è stata effettuata la connesione con successo o meno
         Risposta = conn.getResponseCode();
         
+        //visto che il 200 significa che è tutto ok implemento un controllo basato sul numero resituito
+        if(Risposta != 200){
+            System.out.println("Errore avvenuto ha il codice: " + Risposta);
         
+        } else{
+            StringBuilder infoString = new StringBuilder();
+            Scanner scanner = new Scanner(url.openStream());
+            
+            //aggiungo alla stringa infoString le informazioni dalla risposta;
+            while(scanner.hasNext()){
+                infoString.append(scanner.nextLine());
+            }
+            //va sempre chiusa
+            scanner.close();
+        }
         
-        
-    }
 }
