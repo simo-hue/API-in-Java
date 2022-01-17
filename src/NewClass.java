305f186c5f7cb7a9744caa326b55c420b629ae14
Mattioli Simone <mattioli.simone.10@gmail.com>
2022-01-17 15:09:18 +0100

    main class

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,6 +1,9 @@
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,10 +15,19 @@ import java.net.URL;
  * @author simonemattioli
  */
 public class NewClass {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, IOException {
         //api e link
         //https://www.metaweather.com/api/location/search/?query=<>
         
+        Scanner sc = new Scanner(System.in);
+        String Città = "";
+        
+        //imposto l'URL dalla quale ricevero l'array in formato JSON
         URL url = new URL("https://www.metaweather.com/api/location/search/?query=");
+        
+        //Provo ad instaurare una connessione specificando il metodo della richiesta
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
     }
 }
